8b6237f9bde9256696b8aaa256eb3a88e491c6b9
Nguyen Thanh Tien <tien.thanh.nguyen@skymavis.com>
2024-07-30 17:10:52 +0700

    feat: add wezterm and git config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("Hack Nerd Font Mono")
+config.font_size = 13
+
+config.enable_tab_bar = true
+config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 7
+
+return config
