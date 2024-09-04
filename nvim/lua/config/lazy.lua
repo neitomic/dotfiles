7c9d9b7ad21d68efe3ea5e0a2ccce3931118f491
Tien Nguyen <tien@rocket9.cloud>
2024-09-04 23:13:38 +0700

    chore: add typescript lsp

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     -- import/override with your plugins
     { import = "plugins" },
