5bcf4260ebef1e961a80c3dc580eb78ccbee3165
Tien Nguyen <tien@rocket9.cloud>
2024-10-20 15:10:39 +0700

    rebase

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,12 +3,12 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("0xProto Nerd Font")
-config.font_size = 14
+config.font_size = 13
 
 config.enable_tab_bar = true
 config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.7
 config.macos_window_background_blur = 7
 
 return config
